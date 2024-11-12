81253523fb0f522c2ad8435e732aadce5ec54794
Virtual Jasper <jasper2virtual@hotmail.com>
2024-11-12 15:58:35 +0800

    fix: update attribution link to point to the correct GitHub profile

--- src/components/Attribution.vue
+++ src/components/Attribution.vue
@@ -11,6 +11,6 @@
 <template>
   <div class="attribution">
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-    Coded by <a href="#">Your Name Here</a>.
+    Coded by <a href="https://github.com/jasper2virtual/fem-repo-tip-calculator-app.git">Jasper Lai</a>.
   </div>
 </template>
